92536b53bc7f3e66591e9eae02adc122c6bff737
Pradeep Reddy <easypradeep@outlook.com>
2019-08-12 09:27:42 +0530

    Header and Routes configured

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Projects from './containers/Projects/Projects';
+import Header from './components/Header/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <BrowserRouter>
+        <Route path="/projects" component={Projects}/>
+        <Route path="/users" component={Projects}/>
+        <Route path="/tasks" component={Projects}/>
+        <Route path="/task/details" component={Projects}/>
+      </BrowserRouter>
     </div>
   );
 }
